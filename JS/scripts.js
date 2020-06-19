bf49fc5299e74819881267e656d25b5ba30b959f
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-19 08:51:33 -0700

    add pizza constructor to scripts.js

---
+++ JS/scripts.js
@@ -0,0 +1,21 @@
+function Pizza(size, sauce, toppings) {
+  this.size = size
+  this.sauce = sauce
+  this.toppings = toppings
+  this.totalPrice = 0;
+}
+
+// Pizza.prototype.addToppingsPrice = function() {
+//   this.toppings
+
+// }
+
+// Pizza.prototype.calcTotalPrice = function() {
+//   if(this.totalPrice)
+
+// }
+
+
+$(document).ready(function() {
+
+});
